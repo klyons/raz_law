9c11d221ee1a887ff6f36bfbce7a00df882b9607
Kelly Franklin-Lyons <lyons.kelly@gmail.com>
2024-08-23 18:11:13 -0700

    latest code
    Signed-off-by: Kelly Franklin-Lyons <lyons.kelly@gmail.com>

--- law_api.py
+++ law_api.py
@@ -1,10 +1,12 @@
 # https://justopinion.readthedocs.io/en/latest/guides/getting_started.html#selecting-text-from-an-opinion
 # https://medium.datadriveninvestor.com/legal-and-justice-related-python-libraries-d3888c08768a
-
+import os
 from justopinion import CAPClient
 client = CAPClient(api_token='49eb70bc06a32b6ec66b336ddcb5d68a6d010214')
 #49eb70bc06a32b6ec66b336ddcb5d68a6d010214
 
+
+#examples of how to use the API library
 oracle_download = client.fetch(query="750 F.3d 1339")
 #oracle_case = client.read_cite("750 F.3d 1339", full_case=True)
 
@@ -14,22 +16,33 @@ oracle_data = oracle_download.json()
 oracle_data["results"][0]["name"]
 
 case_names = oracle_data["results"][0]["cites_to"]
+
+
+oracle_download = client.fetch(query="750 F.3d 1339")
+oracle_data["results"][0]["name"]
+
+#make this a function
 citations = []
 for case in case_names:
     citations.append(case["cite"])
 print(citations)
 
-oracle_download = client.fetch(query="750 F.3d 1339")
-oracle_data["results"][0]["name"]
-case_laws = []
-for case in citations:
-    json_obj = client.fetch(query = case)
-    obj = json_obj.json()
-    obj ["results"][0]["name"]
-    case_laws.append(obj ["results"][0]["name"])
 
-    print (case_names [0]["cite"])
+#make a function for this
+def find_citations(citations):
+	case_laws = []
+	for case in citations:
+		json_obj = client.fetch(query = case)
+		obj = json_obj.json()
+		obj ["results"][0]["name"]
+		case_laws.append(obj ["results"][0]["name"])
 
+		print (case_names [0]["cite"])
+	return case_laws
+
+
+
+#everything past here is sorta extraneous
 oracle = client.read_decision_from_response(oracle_download)
 oracle.name_abbreviation
 
@@ -49,9 +62,6 @@ text_selection
 
 parties = thornton.casebody.data.parties[0]
 print(f"Parties: {parties}")
-from justopinion import CAPClient
-
-client = CAPClient(api_token='49eb70bc06a32b6ec66b336ddcb5d68a6d010214')
 
 
 #this is our code example for pulling data from the law library
